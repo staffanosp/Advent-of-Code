0c8d3b5f00fc9178adf1e2db00218db43fc7a20a
Staffan Ösp <staffan.osp@gmail.com>
2022-12-10 10:09:14 +0100

    Day 10 - Part 2

--- 2022/Day-10/Day-10.js
+++ 2022/Day-10/Day-10.js
@@ -12,6 +12,12 @@ const cyclesPerInstrucion = {
   addx: 2,
 };
 
+const printCrt = (crt) => {
+  for (let row of crt) {
+    console.log(row.join(""));
+  }
+};
+
 const solution01 = (inputData) => {
   let x = 1;
   let cycleCounter = 0;
@@ -41,4 +47,37 @@ const solution01 = (inputData) => {
   return;
 };
 
+const solution02 = (inputData) => {
+  let x = 1;
+  let cycleCounter = 0;
+  const crt = [];
+  let crtRow = [];
+
+  //instructions loop
+  for (let [instruction, v] of inputData) {
+    //cycles loop
+    for (let i = 0; i < cyclesPerInstrucion[instruction]; i++) {
+      cycleCounter++;
+
+      let crtX = (cycleCounter - 1) % 40;
+      const spritePosition = [x, x + 1, x - 1];
+      crtRow.push(spritePosition.includes(crtX) ? "#" : " ");
+
+      if (!(cycleCounter % 40)) {
+        crt.push(crtRow);
+        crtRow = [];
+      }
+    }
+
+    //update x
+    if (v) {
+      x += v;
+    }
+  }
+
+  return crt;
+};
+
 console.log(`Solution 01: ${solution01(inputData)}`);
+console.log("Solution 02:");
+printCrt(solution02(inputData));
