e0a5dfb0ef127339b08fde6ee01aa62daca2426a
Staffan Ösp <staffan.osp@gmail.com>
2023-12-13 23:18:29 +0100

    _slight_ cleanup

--- 2023/python/Day-11/Day_11.py
+++ 2023/python/Day-11/Day_11.py
@@ -39,7 +39,7 @@ def solution(galaxies, expansion_multiplier):
     return sum(
         [
             abs(x1 - x2) + abs(y1 - y2)
-            for [(x1, y1), (x2, y2)] in list(combinations(galaxies_expanded, 2))
+            for [(x1, y1), (x2, y2)] in combinations(galaxies_expanded, 2)
         ]
     )
 
